3ba2af05d1543371c5bbd9c3f065e6e709907faa
mdrobny <michdrob@gmail.com>
2015-05-05 23:52:38 +0200

    Remove data mocks and relay on API data for measurements

--- js/controllers/measurements.js
+++ js/controllers/measurements.js
@@ -35,45 +35,21 @@ angular.module('controllers').controller('measurementsController', [
 
 			function getCpuData() {
 				var d = $q.defer();
-				//Measurement.query({type: 'CPU'}, function (data) {
-				//	measurements = data;
-				//    d.resolve();
-				//});
+				Measurement.query({type: 'CPU'}, function (data) {
+					cpuData = data;
+				    d.resolve();
+				});
 
-				// MOCK MOCK
-				cpuData = [{"date":1430828269469,"type":"CPU","value":52},
-					{"date":1430828269470,"type":"CPU","value":83},
-					{"date":1430828269471,"type":"CPU","value":26},
-					{"date":1430828269472,"type":"CPU","value":33},
-					{"date":1430828269473,"type":"CPU","value":18},
-					{"date":1430828269473,"type":"CPU","value":15},
-					{"date":1430828269474,"type":"CPU","value":59,"description":"complex memory"},
-					{"date":1430828269475,"type":"CPU","value":2,"description":"complex cpu"},
-					{"date":1430828269476,"type":"CPU","value":42,"description":"complex network"}];
-
-				d.resolve();
 				return d.promise;
 			}
 
 			function getMemoryData() {
 				var d = $q.defer();
-				//Measurement.query({type: 'MEMORY'}, function (data) {
-				//	memoryData = data;
-				//    d.resolve();
-				//});
+				Measurement.query({type: 'MEMORY'}, function (data) {
+					memoryData = data;
+				    d.resolve();
+				});
 
-				// MOCK MOCK
-				memoryData = [{"date":1430828269477,"type":"MEMORY","value":81},
-					{"date":1430828269477,"type":"MEMORY","value":18},
-					{"date":1430828269478,"type":"MEMORY","value":13},
-					{"date":1430828269479,"type":"MEMORY","value":23},
-					{"date":1430828269480,"type":"MEMORY","value":18},
-					{"date":1430828269480,"type":"MEMORY","value":61},
-					{"date":1430828269481,"type":"MEMORY","value":78,"description":"complex memory"},
-					{"date":1430828269482,"type":"MEMORY","value":95,"description":"complex cpu"},
-					{"date":1430828269483,"type":"MEMORY","value":51,"description":"complex network"}];
-
-				d.resolve();
 				return d.promise;
 			}
 
